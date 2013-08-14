1a68112fed630a22e782d1b731e977123bca5d8e
mikesmithjr <mikesmithjr@fullsail.edu>
2013-08-13 22:37:54 -0500

    Adding content provider and setting up UriMatching

--- Week 2/MovieListing/gen/com/wickedsoftwaredesigns/movielisting/R.java
+++ Week 2/MovieListing/gen/com/wickedsoftwaredesigns/movielisting/R.java
@@ -29,21 +29,23 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000e;
-        public static final int data_rating=0x7f090006;
-        public static final int data_runtime=0x7f090005;
-        public static final int data_thumbnail=0x7f090007;
-        public static final int data_title=0x7f090004;
-        public static final int moviedata=0x7f090003;
-        public static final int movielist=0x7f090002;
-        public static final int rating=0x7f09000c;
-        public static final int rating_header=0x7f090009;
-        public static final int runtime=0x7f09000d;
-        public static final int runtime_header=0x7f09000a;
+        public static final int action_settings=0x7f090010;
+        public static final int data_rating=0x7f090008;
+        public static final int data_runtime=0x7f090007;
+        public static final int data_thumbnail=0x7f090009;
+        public static final int data_title=0x7f090006;
+        public static final int filterButton=0x7f090003;
+        public static final int filterField=0x7f090002;
+        public static final int moviedata=0x7f090005;
+        public static final int movielist=0x7f090004;
+        public static final int rating=0x7f09000e;
+        public static final int rating_header=0x7f09000b;
+        public static final int runtime=0x7f09000f;
+        public static final int runtime_header=0x7f09000c;
         public static final int searchButton=0x7f090001;
         public static final int searchField=0x7f090000;
-        public static final int title=0x7f09000b;
-        public static final int title_header=0x7f090008;
+        public static final int title=0x7f09000d;
+        public static final int title_header=0x7f09000a;
     }
     public static final class layout {
         public static final int form=0x7f030000;
@@ -57,6 +59,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int filter=0x7f050010;
+        public static final int filter_your_movie_list=0x7f050011;
         public static final int go=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int movie_title_=0x7f050007;
